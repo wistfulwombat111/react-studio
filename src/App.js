8d4aa4df7769c628c16ffb8eb79546d79d33bf99
wistfulwombat111 <114682470+wistfulwombat111@users.noreply.github.com>
2022-11-10 18:38:34 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
+import CartItem from "./components/CartItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +13,33 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  var [cartVal, setCartVal] = useState(0.00);
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Gourmet Hound Bakery</h1>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div className="Menu">
+        <h2>Menu</h2>
+        <div className="MenuWrapper">
+          {bakeryData.map((item, index) => (
+            <BakeryItem itemName={item.name} description={item.description}
+              price={item.price} imgURL={item.image} itemIndex={index}
+              cart={cart} setCart={setCart} cartVal={cartVal}
+              setCartVal={setCartVal}/>
+          ))}
+        </div>
+      </div>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <div className="Cart">
+        <h2>Basket</h2>
+
+        {bakeryData.map((item, index) => (
+          <CartItem count={cart[index]} itemName={item.name}/>
+        ))}
+
+        <h2>Total: ${cartVal}</h2>
       </div>
     </div>
   );
